7618ae111696db0d53564cb3ffcdce8b814b5f99
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-07 15:19:39 +0500

    add content and styling for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from '../utils/constants';
+import SearchBar from "./SearchBar";
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItem="center"
+    p={2}
+    sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
+  >
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+)
 
 export default Navbar
