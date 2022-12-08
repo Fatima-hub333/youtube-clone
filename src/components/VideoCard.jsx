1bbdedbc5dec5a30c5ab9d4dddc7da4d8346305b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-08 16:41:16 +0500

    created VideoCard & ChannelCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle} from '../utils/constants';
 
-const VideoCard = () => {
+
+const VideoCard = ({ video: { id: {
+  videoId }, snippet}}) => {
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+    </Card>
   )
 }
 
