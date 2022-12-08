fae0132c28f46a4d90f8cb043061fe5d69ec0176
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-08 14:35:58 +0500

    add sidebar content & styling

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
   return (
@@ -7,11 +8,30 @@ const Feed = () => {
       flexDirection: {
         sx: "column", md: "row" }}}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        Sidebar 
-
+        <Sidebar />
         <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
           Copyright 2022 FemSol Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{
+        overflowY: 'auto',
+        height: '90vh',
+        flex: 2
+      }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2} sx={{
+            color: 'white'
+          }}
+        >
+          New <span style={{
+            color: '#F31503'
+          }}>Videos</span>
+        </Typography>
+
+        <Videos />
       </Box>
     </Stack>
   )
