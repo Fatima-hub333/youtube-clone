8426bf75c82093667f5ccd25ecb27af46ff642c7
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-12 10:46:04 +0500

    update channelDetail file

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -14,11 +14,8 @@ const ChannelDetail = () => {
   useEffect(() => {
     const fetchResults = async () => {
       const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
-
       setChannelDetail(data?.items[0]);
-
       const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
-
       setVideos(videosData?.items);
     };
 
