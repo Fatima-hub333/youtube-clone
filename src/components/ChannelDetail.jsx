b366dffb1a02a5d0d412cdb079ac7ee62961390a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-12 10:24:32 +0500

    Update Functions

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useState, useeffect, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {Videos, ChannelCard} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+  }, [id])
   return (
     <div>ChannelDetail</div>
   )
